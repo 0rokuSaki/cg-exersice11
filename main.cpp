7fc766e8f32a5db853d5758c80213f550dbe06a8
BAAARON2 <BAAARON2@365.openu.ac.il>
2023-03-17 16:48:44 +0200

    fixed exit button and title

--- main.cpp
+++ main.cpp
@@ -20,36 +20,41 @@
 #include <string>
 #include <math.h>
 
-/* General constants */
-#define FONT_SWITCH_WIDTH  500  // Window  width at which font changes size
-#define FONT_SWITCH_HEIGHT 300  // Window height at which font changes size
 
 /* Window constants */
-#define WINDOW_X_OFFSET 100            // Initial window left-offset
-#define WINDOW_Y_OFFSET 100            // Initial window top-offset
-#define WINDOW_HEIGHT   500            // Initial window height
-#define WINDOW_WIDTH    500            // Initial window width
+#define WINDOW_X_OFFSET 100u           // Initial window left-offset
+#define WINDOW_Y_OFFSET 100u           // Initial window top-offset
+#define WINDOW_HEIGHT   500u           // Initial window height
+#define WINDOW_WIDTH    500u           // Initial window width
 #define WINDOW_TITLE    "CG Maman 11"  // Window title
 #define X_COORD_RNG     100.0f         // Max value for x coord
 #define Y_COORD_RNG     100.0f         // Max value for y coord
 
 /* Exit button constants */
-#define EXIT_BTN_HEIGHT            10.0f   // Exit button height
-#define EXIT_BTN_WIDTH             20.0f   // Exit button width
-#define EXIT_BTN_TEXT_RASTER_POS_X 3.0f    // Exit button text x coord
-#define EXIT_BTN_TEXT_RASTER_POS_Y 3.0f    // Exit button text y coord
-#define EXIT_BTN_TEXT              "EXIT"  // Exit button text
+#define EXIT_BTN_WIDTH       85u     // Exit button  width (pixel)
+#define EXIT_BTN_HEIGHT      50u     // Exit button height (pixel
+#define EXIT_BTN_TEXT_POS_X  14.0f   // Exit button text x coord
+#define EXIT_BTN_TEXT_POS_Y  15.0f   // Exit button text y coord
+#define EXIT_BTN_TEXT        "EXIT"  // Exit button text
+#define EXIT_BTN_FRAME_WIDTH 3.0f    // Exit button frame width
 
 /* Title constants */
-#define TITLE_HEIGHT              17.5f            // Title height
-#define TITLE_WIDTH               35.0f            // Title width
-#define TITLE_TEXT_RASTER_POS_X   66.0f            // Title text x coord
-#define TITLE_TEXT_RASTER_POS_Y   92.5f            // Title text y coord
-#define TITLE_AUTHOR_RASTER_POS_X 66.0f            // Title author x coord
-#define TITLE_AUTHOR_RASTER_POS_Y 85.0f            // Title author y coord
-#define TITLE_TEXT                "CG - Maman 11"  // Title text
-#define TITLE_AUTHOR              "Aaron Barkan"   // Title author name
-#define TITLE_LINE_WIDTH          3.0f             // Title frame line width
+#define TITLE_HEIGHT       75u                     // Title height
+#define TITLE_WIDTH        175u                    // Title width
+#define TITLE_TEXT_POS_X   5.0f                    // Title text x coord
+#define TITLE_TEXT_POS_Y   50.0f                   // Title text y coord
+#define TITLE_AUTHOR_POS_X 5.0f                    // Title author x coord
+#define TITLE_AUTHOR_POS_Y 25.0f                   // Title author y coord
+#define TITLE_TEXT         "CG - Maman 11"         // Title text
+#define TITLE_AUTHOR       "Author: Aaron Barkan"  // Title author name
+#define TITLE_FRAME_WIDTH  3.0f                    // Title frame width
+
+/* Logo constants */
+#define LOGO_LINE1_X 30.0f
+#define LOGO_LINE2_X 70.0f
+#define LOGO_LINE_Y1 40.0f
+#define LOGO_LINE_Y2 70.0f
+#define LOGO_TEXT    "OpenU"
 
 
 /* HELPER FUNCTIONS */
@@ -71,72 +76,98 @@ void createWindow(int argc, char** argv)
 }
 
 
-/* Get font according to window size */
-void* getFont(const std::string& fontName)
+void createFrame(const int width, const int height, const int frameWidth)
 {
-    void* font = nullptr;
-    bool large = (glutGet(GLUT_WINDOW_WIDTH) >= FONT_SWITCH_WIDTH) && (glutGet(GLUT_WINDOW_HEIGHT) >= FONT_SWITCH_HEIGHT);
-    if ("Times Roman" == fontName)
-    {
-        font = large ? GLUT_BITMAP_TIMES_ROMAN_24 : GLUT_BITMAP_TIMES_ROMAN_10;
-    }
-    else if ("Halvetica" == fontName)
-    {
-        font = large ? GLUT_BITMAP_HELVETICA_18 : GLUT_BITMAP_HELVETICA_10;
-    }
-    else
-    {
-        std::cout << "[getFont] Invalid font name: " << fontName << std::endl;
-    }
-    return font;
+    glLineWidth(frameWidth);
+    const float offset = frameWidth / 2.0;  // To see frame correctly
+    glBegin(GL_LINE_LOOP);
+        glVertex2f(0.0f + offset, 0.0f + offset);
+        glVertex2f(width - offset, 0.0f + offset);
+        glVertex2f(width - offset, height - offset);
+        glVertex2f(0.0f + offset, height - offset);
+    glEnd();
 }
 
 
 /* Displays an exit button at the bottom left of the window */
 void displayExitButton(void)
 {
+    /* Create viewport for exit button */
+    glViewport(0, 0, EXIT_BTN_WIDTH, EXIT_BTN_HEIGHT);
+    glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0.0, EXIT_BTN_WIDTH, 0.0, EXIT_BTN_HEIGHT);
+
     /* Create red rectangle at the bottom left */
     glColor3f(1.0f, 0.0f, 0.0f);
     glRectf(0.0f, 0.0f, EXIT_BTN_WIDTH, EXIT_BTN_HEIGHT);
 
+    /* Create black frame */
+    glColor3f(0.0f, 0.0f, 0.0f);
+    createFrame(EXIT_BTN_WIDTH, EXIT_BTN_HEIGHT, EXIT_BTN_FRAME_WIDTH);
+
     /* Add text to button */
     glColor3f(0.0f, 0.0f, 0.0f);
-    void* font = getFont("Times Roman");
-    glRasterPos2f(EXIT_BTN_TEXT_RASTER_POS_X, EXIT_BTN_TEXT_RASTER_POS_Y);
-    for (char c : EXIT_BTN_TEXT)
+    glRasterPos2f(EXIT_BTN_TEXT_POS_X, EXIT_BTN_TEXT_POS_Y);
+    for (const char c : EXIT_BTN_TEXT)
     {
-        glutBitmapCharacter(font, c);
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, c);
     }
 }
 
 
+/* Display title and author name at the top right of the window */
 void displayTitle(void)
 {
-    /* Create a black frame at the top right */
+    /* Create viewport for title */
+    const int viewportX = glutGet(GLUT_WINDOW_WIDTH) - TITLE_WIDTH;
+    const int viewportY = glutGet(GLUT_WINDOW_HEIGHT) - TITLE_HEIGHT;
+    glViewport(viewportX, viewportY, TITLE_WIDTH, TITLE_HEIGHT);
+    glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0.0, TITLE_WIDTH, 0.0, TITLE_HEIGHT);
+
+    /* Create a black frame */
     glColor3f(0.0f, 0.0f, 0.0f);
-    glLineWidth(TITLE_LINE_WIDTH);
-    glBegin(GL_LINE_LOOP);
-        glVertex2f(X_COORD_RNG - TITLE_WIDTH, Y_COORD_RNG - TITLE_HEIGHT);
-        glVertex2f(X_COORD_RNG, Y_COORD_RNG - TITLE_HEIGHT);
-        glVertex2f(X_COORD_RNG, Y_COORD_RNG);
-        glVertex2f(X_COORD_RNG - TITLE_WIDTH, Y_COORD_RNG);
-    glEnd();
+    createFrame(TITLE_WIDTH, TITLE_HEIGHT, TITLE_FRAME_WIDTH);
 
     /* Create title text */
-    void* font = getFont("Times Roman");
-    glRasterPos2f(TITLE_TEXT_RASTER_POS_X, TITLE_TEXT_RASTER_POS_Y);
-    for (char c : TITLE_TEXT)
+    glRasterPos2f(TITLE_TEXT_POS_X, TITLE_TEXT_POS_Y);
+    for (const char c : TITLE_TEXT)
     {
-        glutBitmapCharacter(font, c);
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, c);
     }
 
     /* Create title author */
-    font = getFont("Halvetica");
-    glRasterPos2f(TITLE_AUTHOR_RASTER_POS_X, TITLE_AUTHOR_RASTER_POS_Y);
-    for (char c : TITLE_AUTHOR)
+    glRasterPos2f(TITLE_AUTHOR_POS_X, TITLE_AUTHOR_POS_Y);
+    for (const char c : TITLE_AUTHOR)
     {
-        glutBitmapCharacter(font, c);
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, c);
     }
+}
+
+
+/* Display logo at the center of the window */
+void displayLogo(void)
+{
+    /* Get window dimensions */
+    const int windowWidth = glutGet(GLUT_WINDOW_WIDTH);
+    const int windowHeight = glutGet(GLUT_WINDOW_HEIGHT);
+    
+    /* Create a viewport at the center of the window */
+    glViewport(windowWidth / 4, windowHeight / 4, windowWidth / 2, windowHeight / 2);
+
+    /* Draw a blue logo of OpenU */
+    glColor3f(0.0f, 0.0f, 0.8f);
+    glBegin(GL_LINES);
+        glVertex2f(LOGO_LINE1_X, LOGO_LINE_Y1);
+        glVertex2f(LOGO_LINE1_X, LOGO_LINE_Y2);
+    glEnd();
+
+    glBegin(GL_LINES);
+        glVertex2f(LOGO_LINE2_X, LOGO_LINE_Y1);
+        glVertex2f(LOGO_LINE2_X, LOGO_LINE_Y2);
+    glEnd();
 }
 
 
@@ -146,25 +177,22 @@ void displayTitle(void)
 void displayCallback(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
     displayExitButton();
     displayTitle();
+    displayLogo();
+
     glFlush();
 }
 
 
 /* Callback for 'glutMouseFunc' */
-void mouseCallback(int button, int state, int _x, int _y)
+void mouseCallback(int button, int state, int x, int y)
 {
-    /* Transform mouse coordinates to Ortho 2D coordinates */
-    const long long windowHeight = glutGet(GLUT_WINDOW_HEIGHT);
-    const double x = _x * X_COORD_RNG / glutGet(GLUT_WINDOW_WIDTH);
-    const double y = (windowHeight - _y) * Y_COORD_RNG / windowHeight;
-
     /* Exit program if right click on exit button */
     if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN &&
-        x <= EXIT_BTN_WIDTH && y <= EXIT_BTN_HEIGHT)
+        x < EXIT_BTN_WIDTH && y >= glutGet(GLUT_WINDOW_HEIGHT) - EXIT_BTN_HEIGHT)
     {
-        glutDestroyWindow(1);
         exit(0);
     }
 }
